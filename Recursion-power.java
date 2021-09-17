62a80ba061093d8702aa16f11dd3e007785a8d7b
Vishwas Sharma <78073978+vishxwas13@users.noreply.github.com>
2021-09-17 23:45:55 +0530

    Update Recursion-power.java

--- Recursion-power.java
+++ Recursion-power.java
@@ -5,14 +5,21 @@ import java.util.*;
 class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int x = scan.nextInt();
-        int n = scan.nextInt();
-        int ans = 1;
-        while (n>0){
-            ans*= x;
-            n--;
-        }
-        System.out.println(ans);
+        int num = scan.nextInt();
+        int power = scan.nextInt();
+        int result = 1;
+        System.out.println(pow(num,power));
 
+    }
+    public static int pow(int m,int n){
+        if (n==0){
+            return 1;
+        }
+        if (n%2==0){
+            return pow(m*m,n/2);
+        }
+        else {
+            return m*(pow(m*m,(n-1)/2));
+        }
     }
 }
